50a16508a176d32b587e29ad79cb471726c48636
shahnawaz-saleem <fingertips.shahnawaz@gmail.com>
2026-03-09 07:59:03 +0530

    test

--- src/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
+++ src/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71abd64f3d002ed9322682bf79bf3095a448db09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6534b8957219d8c73c4695af03cd5ed1057db8a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
