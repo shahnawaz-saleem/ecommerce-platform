e15ef26cc0ad25df8e60f650b31efe61676eb50b
shahnawaz-saleem <fingertips.shahnawaz@gmail.com>
2026-03-10 01:45:20 +0530

    git add .

--- src/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
+++ src/Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daae8a7283d576d2e0b1ebda97feca216e776d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbe48709aaad657da1eac41011caa5e5252eefe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Ecommerce.Infrastructure/obj/Debug/net8.0/Ecommerce.Infrastructure.AssemblyInfo.cs
+++ src/Ecommerce.Infrastructure/obj/Debug/net8.0/Ecommerce.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a1bc6da018c70e5b53705af8f75081344631fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbe48709aaad657da1eac41011caa5e5252eefe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
