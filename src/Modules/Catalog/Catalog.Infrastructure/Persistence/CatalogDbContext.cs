90a6c04b0fbf2c443baa76da211d97f7a0d25c10
shahnawaz-saleem <fingertips.shahnawaz@gmail.com>
2026-03-11 07:12:56 +0530

    Added EfCore in infrastructure layer

---
+++ src/Modules/Catalog/Catalog.Infrastructure/Persistence/CatalogDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace Catalog.Infrastructure.Persistence;
+
+public class CatalogDbContext : DbContext
+{
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+
+            entity.Property(p => p.Name)
+                  .IsRequired()
+                  .HasMaxLength(200);
+
+            entity.Property(p => p.Price)
+                  .HasColumnType("decimal(18,2)");
+
+            entity.Property(p => p.Description)
+                  .HasMaxLength(1000);
+        });
+    }
+}
